1688e1e36eb4b5629003dc2256a44985580b4344
Jordy van Zeeland <jordyvanzeeland@gmail.com>
2021-04-11 20:38:58 +0200

    CRUD Projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -11,14 +11,14 @@ class ProjectsController extends Controller
 {
 
 	public function encrypt_decrypt($action, $string) {
+
 		$output = false;
 		$encrypt_method = "AES-256-CBC";
 		$secret_key = 'This is my secret key';
 		$secret_iv = 'This is my secret iv';
 		// hash
 		$key = hash('sha256', $secret_key);
-	   
-		// iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+
 		$iv = substr(hash('sha256', $secret_iv), 0, 16);
 		if ( $action == 'encrypt' ) {
 			$output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
@@ -40,31 +40,30 @@ class ProjectsController extends Controller
     public function details($Id){
 
 		$Project = Project::find($Id);
-
-		$ProjectPasswords = DB::table('ldeq_passwords')
-					->select('ldeq_passwords.type', 'ldeq_types.type', 'ldeq_passwords.host', 'ldeq_passwords.username', 'ldeq_passwords.password')
-					->join('ldeq_types', 'ldeq_passwords.type', '=', 'ldeq_types.id')
-					->where('ldeq_passwords.projectid', $Id)
-					->get();
-
-		foreach($ProjectPasswords as $password){
-			$password->password = $this->encrypt_decrypt('decrypt', $password->password);
-		}
-		
-
-    	return ['data' => $Project, 'passwords' => $ProjectPasswords];
+		$Project->FtpPass = $this->encrypt_decrypt('decrypt', $Project->FtpPass);
+		$Project->DbPass = $this->encrypt_decrypt('decrypt', $Project->DbPass);
+		$Project->WpPass = $this->encrypt_decrypt('decrypt', $Project->WpPass);
+    	return ['data' => $Project];
 
     }
 
     public function create(Request $Request){
 
-		// $EncryptedFtpPass = $this->encrypt_decrypt('encrypt', $Request->FtpPass);
-		// $EncryptedDbPass = $this->encrypt_decrypt('encrypt', $Request->DbPass);
-		// $EncryptedWpPass = $this->encrypt_decrypt('encrypt', $Request->WpPass);
+		$EncryptedFtpPass = $this->encrypt_decrypt('encrypt', $Request->header('projectftppass'));
+		$EncryptedDbPass = $this->encrypt_decrypt('encrypt', $Request->header('projectdbpass'));
+		$EncryptedWpPass = $this->encrypt_decrypt('encrypt', $Request->header('projectwppass'));
 
 		$Project = DB::table('ldeq_projects')->insert([
-			'projectname' => $Request->header('projectname'),
-			'projecturl' => $Request->header('projecturl'),
+			'ProjectName' => $Request->header('projectname'),
+			'ProjectUrl' => $Request->header('projecturl'),
+			'FtpHost' => $Request->header('projectftphost'),
+			'FtpUser' => $Request->header('projectftpuser'),
+			'FtpPass' => $EncryptedFtpPass,
+			'DbHost' => $Request->header('projectdbhost'),
+			'DbUser' => $Request->header('projectdbuser'),
+			'DbPass' => $EncryptedDbPass,
+			'WpUser' => $Request->header('projectwpuser'),
+			'WpPass' => $EncryptedWpPass
 		]);
 
     	return response()->json($Project, 201);
@@ -73,8 +72,24 @@ class ProjectsController extends Controller
 
     public function update(Request $Request, $Id){
 
-    	$Project = Project::findOrFail($Id);
-    	$Project->update($Request->all());
+		$EncryptedFtpPass = $this->encrypt_decrypt('encrypt', $Request->header('projectftppass'));
+		$EncryptedDbPass = $this->encrypt_decrypt('encrypt', $Request->header('projectdbpass'));
+		$EncryptedWpPass = $this->encrypt_decrypt('encrypt', $Request->header('projectwppass'));
+
+		$Project = DB::table('ldeq_projects')->where('Id', $Id)->update([
+			'ProjectName' => $Request->header('projectname'),
+			'ProjectUrl' => $Request->header('projecturl'),
+			'FtpHost' => $Request->header('projectftphost'),
+			'FtpUser' => $Request->header('projectftpuser'),
+			'FtpPass' => $EncryptedFtpPass,
+			'DbHost' => $Request->header('projectdbhost'),
+			'DbUser' => $Request->header('projectdbuser'),
+			'DbPass' => $EncryptedDbPass,
+			'WpUser' => $Request->header('projectwpuser'),
+			'WpPass' => $EncryptedWpPass
+		]);
+
+		// $Project->update($Request->all());
 
     	return response()->json($Project, 200);
 
